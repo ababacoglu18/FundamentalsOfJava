88f507b0d64d126101312658636451d7f9681c34
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-05-07 04:39:21 +0300

    .1

--- src/fordongusu.java
+++ src/fordongusu.java
@@ -3,7 +3,47 @@ import java.util.Scanner;
 public class fordongusu {
     public static void main(String[] args){
 
+int i = 0;
+int j = 10;
+     for (; i < 10 && j > 0; i++,j--){         //Don't forget ; in first step.
+         System.out.println("i = " + i + " j = " + j);
+     }
+
+        System.out.println("\n");
+//--------------------------------------------------------------------------
+     for(int a = 2; a < 100; a *= 2){
+         System.out.println("a : " + a);
+     }
+
+        System.out.println("\n");
+//--------------------------------------------------------------------------
+//Faktoriyel Islemi
+
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Faktoriyel Sayisi Gİriniz: ");
+
+        int faktoriyelToplam =  1;
+
+        int sayi = scanner.nextInt();
+
+
+
+        for(int c = 1; c <= sayi; c++){
+
+            faktoriyelToplam *= c;
+
+
+        }
+        if(sayi>0) {
+            System.out.println("Faktoriyel = " + faktoriyelToplam);
+        }
+        else{
+            System.out.println("Hatali Islem...");
+
+        }
+
+
 
 
     }
