75425ca1803d6b8cb376dc8c4cb61c1d5bbc6be9
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-05-08 17:36:35 +0300

    .3

---
+++ src/BankApplication.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BankApplication {
+    public static void main(String args){
+        Scanner scanner = new Scanner(System.in);
+
+        int bakiye  = 1000;
+        String islemler = "1. Islem: Bakiye Ogrenme \n"
+                +           "2. Islem: Para Çekme\n"
+                +           "3. Islem: Para Yatırma\n"
+                +           "Çıkış için q ya basın";
+        System.out.println("**********************************");
+        System.out.println(islemler);
+        System.out.println("**********************************");
+
+
+
+
+
+
+
+
+}
+}
