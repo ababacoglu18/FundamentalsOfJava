1c2ba22ca5d0d07eae7c13bab41767ddf896b048
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-05-10 19:36:56 +0300

    .5

---
+++ src/_2Return.java
@@ -0,0 +1,8 @@
+public class _2Return {
+
+
+    public static void main(String[] args){
+
+
+    }
+}
--- src/_2UsingParams.java
+++ src/_2UsingParams.java
@@ -1,7 +1,18 @@
 public class _2UsingParams {
 
+        public static void sum(int a,int b, int c){
+            System.out.println("Sum: " + (a + b +c));
+        }
+        public static void selam(String name){
+
+            System.out.println("Hi... " + name);
+        }
+
     public static void main(String[] args){
 
+        selam("Json. ");
+
+        sum(3,4,5);
 
     }
 }
