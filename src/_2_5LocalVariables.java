aab590f1485fbe231af9e2997766ae0f30e0c5fe
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-05-11 13:59:43 +0300

    1.0

--- src/_2_5LocalVariables.java
+++ src/_2_5LocalVariables.java
@@ -2,6 +2,12 @@ public class _2_5LocalVariables {
 
 
 
+    public static void sum(int z, int b, int c){
+
+
+        System.out.println("Result: "  + (z + b +c ));
+    }
+
 
     public static void main(String[] args){
         int a =4;            //variable a connected with main method.
@@ -11,12 +17,22 @@ public class _2_5LocalVariables {
             int b = 5;           //b is local variable for if block.
             System.out.println(print);
         }
+        //********************************************************************\\
+
+        int d = 1;
+
+        while (d < 10){
+
+            if(d%2 == 0){
+                    String print2 = "Even " + d;
+                System.out.println(print2);
+            }
+                    d++;
+            //*************************************************************\\
 
 
-        for(int i = 0; i<10;i++){
-
-            System.out.println(i); // i local for i
+            sum(10,20,30);  //
+          // System.out.println(z);   //no funcionará porque la variable z está definida solo para la función de suma
         }
-       // System.out.println(i); it wont work.
     }
 }
