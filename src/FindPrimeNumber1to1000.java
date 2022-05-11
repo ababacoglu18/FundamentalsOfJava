30a33fff5ddd34c4acea56156800de63daef9ab7
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-05-11 14:14:01 +0300

    1.0

---
+++ src/FindPrimeNumber1to1000.java
@@ -0,0 +1,22 @@
+public class FindPrimeNumber1to1000 {
+
+
+    public static boolean isPrimeNumber(int number){
+        for(int i =2 ; i < number; i++){
+
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+   for(int i = 2; i<=1000; i++){
+
+   if (isPrimeNumber(i)){
+       System.out.println(i);
+
+   }
+}
+    }
+}
