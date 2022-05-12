9ed37dcad68286b03f58387a3b8e594ba09be807
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-05-12 23:45:33 +0300

    1.3

---
+++ src/com/car2/carFunction.java
@@ -0,0 +1,22 @@
+package com.car2;
+
+public class carFunction {
+
+
+    public static void main(String[] args){
+
+        style car1 = new style();
+
+
+        car1.color = "Kırmızı";
+        car1.engineType = "16V";
+        car1.numberOfDoors = 4;
+        car1.model = "520i";
+
+
+        System.out.println(" " + car1.color + "\n " + car1.engineType + "\n " + car1.numberOfDoors + "\n " + car1.model);
+
+
+
+    }
+}
---
+++ src/com/car2/style.java
@@ -0,0 +1,9 @@
+package com.car2;
+
+public class style {
+
+    public String color;
+    public String engineType;
+    public int    numberOfDoors;
+    public String model;
+}
